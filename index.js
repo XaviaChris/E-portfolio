688ae4d43229135149818bf80649c74212fcf4d6
Xavia Christopher <xaviachristopher@Xavias-MacBook-Air.local>
2026-03-22 22:24:36 -0400

    all major changes done

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-template_byiaebq
-service_bpdx6ho
-tHypjaTPZCFUZ-QEe
+// template_byiaebq
+// service_bpdx6ho
+// tHypjaTPZCFUZ-QEe
+
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    console.log(x, y)
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x *  boolInt}px, ${y *  boolInt}px)`
+    }
+
+}
+
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+     document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove ("dark-theme")
+    }
+  
+}
 
 function contact(event) {
     event.preventDefault();
@@ -27,12 +58,13 @@ function contact(event) {
 
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
    if (isModalOpen) {
         isModalOpen = false;
         return document.body.classList.remove("modal--open");
    }
+
      isModalOpen = true;
     document.body.classList += " modal--open";
 }
